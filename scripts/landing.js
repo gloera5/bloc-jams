7b52b18f2f55d51970b6b131934d79f7bd2d4eb8
gloera5 <gloera5@live.com>
2016-03-08 04:46:49 +0000

    checkpoint 29 complete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,33 +1,27 @@
-var pointsArray = document.getElementsByClassName('point');
- 
-var animatePoints = function(points) {
+var animatePoints = function() {
  
      var revealPoint = function(i){
-         points[i].style.opacity = 1;
-         points[i].style.transform = "scaleX(1) translateY(0)";
-         points[i].style.msTransform = "scaleX(1) translateY(0)";
-         points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+         var revealPoint = function() {
      };
-
-     for (var i=0; i < points.length; i++) {
-       revealPoint(i);
-     }
+     $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
+     $.each($('.point'), revealPoint);
 };
 
-window.onload = function() {
+ $(window).load(function() {
   
-  if (window.innerHeight > 950) {
-         animatePoints(pointsArray);
-     }
+  if ($(window).height() > 950) {
+         animatePoints();
   
-  var sellingPoints = document.getElementsByClassName('selling-points')[0];
-  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
   
-  window.addEventListener('scroll', function(event) {
+  $(window).scroll(function(event) {
          console.log("Current offset from the top is " + sellingPoints.getBoundingClientRect().top + " pixels");
-         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-             animatePoints(pointsArray);   
+         if ($(window).scrollTop() >= scrollDistance) {
+             animatePoints();   
          }
      });
-}
+});
 
