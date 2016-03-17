425b050b66caf1cc1a7a7baa9f8dc13e004826ec
gloera5 <gloera5@live.com>
2016-03-17 02:44:38 +0000

    assignment 33 complete

--- scripts/album.js
+++ scripts/album.js
@@ -27,7 +27,7 @@ var createSongRow = function(songNumber, songName, songLength) {
        '<tr class="album-view-song-item">'
        '  <td class="song-item-number" data-song-number="' + songNumber + '">' + songNumber + '</td>'
        '  <td class="song-item-title">' + songName + '</td>'
-       '  <td class="song-item-duration">' + songLength + '</td>'
+       '  <td class="song-item-duration">' + filertTimeCode(songLength) + '</td>'
        '</tr>'
      ;
   
@@ -192,7 +192,6 @@ var nextSong = function() {
  var playerBarPauseButton = '<span class="ion-pause"></span>';
  var updatePlayerBarSong = function() {
 
-     $('.main-controls .play-pause').html(playerBarPauseButton);
  };
 
  var currentlyPlayingSongNumber = null;
@@ -208,13 +207,42 @@ var nextSong = function() {
          currentSoundFile.setTime(time);
      }
  }
+ 
+ var setCurrentTimeInPlayerBar = function(currentTime) {
+   var $setCurrentTimeElement = $('.seek-control .current-time');
+   $currentTimeElement.text(currentTime);
+ };
+ 
+ var setTotalTimeInPlayerBar = function(totalTime) {
+   var $totalTimeElement = $('.seek-control .total-time');
+   $totalTimeElement.text(totalTime);
+ };
+
+ var filterTimeCode = function(timeInSeconds) {
+   var seconds = Number.parseFloat(timeInSeconds);
+   var wholeSeconds = Math.floor(seconds);
+   var minutes = Math.floor(wholeSeconds/60);
+   var remainingSeconds = wholeSeconds % 60;
+   
+   var output = minutes + ':';
+   if (remainingSeconds < 10) {
+     output += '0';
+   }
+   output += remainingSeconds;
+   return output;
+   
+ };
+
  var updateSeekBarWhileSongPlays = function() {
      if (currentSoundFile) {
        currentSoundFile.bind('timeupdate', function(event) {
+         var currentTime = this.getTime();
+         var songLength = this.getDuration();
          var seekBarFillRatio = this.getTime() / this.getDuration();
              var $seekBar = $('.seek-control .seek-bar');
  
              updateSeekPercentage($seekBar, seekBarFillRatio);
+             setCurrentTimeInPlayerBar(currentTime);
          });
      }
  };
@@ -248,6 +276,7 @@ var nextSong = function() {
              var seekBarFillRatio = offsetX / barWidth;
  
              updateSeekPercentage($seekBar, seekBarFillRatio);
+             setCurrentTimeInPlayerBar(filterTimeCode(currentTime));
          });
        $(document).bind('mouseup.thumb', function() {
              $(document).unbind('mousemove.thumb');
@@ -255,6 +284,16 @@ var nextSong = function() {
          });
      });
  };
+
+ var updatePlayerBarSong = function() {
+   $('.currently-playing .song-name').text(currentSongFromAlbum.name);
+   $('.currently-playing .artist-name').text(currentAlbum.artist);
+   $('.currently-playing .artist-song-mobile').text(currentSongFromAlbum.name) + " - " + currentAlbum.artist);
+   $('.main-controls .play-pause').html(playerBarPauseButton);
+   setTotalTimeInPlayerBar(filterTimeCode(currentSongFromAlbum.length));
+ }
+ 
+   
  
  $seekBars.click(function(event) {
         var offsetX = event.pageX - $(this).offset().left;
